75f9d9f2c8eb118d5f5480c9d27583e923a581bd
David Hao <david.hao@addepar.com>
2017-06-09 15:35:09 -0400

    Base

--- test/com/facebook/buck/rules/ProjectConfigBuilder.java
+++ test/com/facebook/buck/rules/ProjectConfigBuilder.java
@@ -42,12 +42,6 @@ public class ProjectConfigBuilder extends AbstractNodeBuilder<ProjectConfigDescr
     return this;
   }
 
-  public ProjectConfigBuilder setSrcResourceRoots(@Nullable ImmutableList<String> srcResourceRoots)
-  {
-    arg.srcResourceRoots = Optional.fromNullable(srcResourceRoots);
-    return this;
-  }
-
   public ProjectConfigBuilder setTestRule(@Nullable BuildTarget testRule) {
     arg.testTarget = Optional.ofNullable(testRule);
     return this;
@@ -58,19 +52,13 @@ public class ProjectConfigBuilder extends AbstractNodeBuilder<ProjectConfigDescr
     return this;
   }
 
-  public ProjectConfigBuilder setTestResourceRoots(@Nullable ImmutableList<String>
-      testResourceRoots) {
-    arg.testResourceRoots = Optional.fromNullable(testResourceRoots);
-    return this;
-  }
-
   public ProjectConfigBuilder setJdkName(@Nullable String jdkName) {
-    arg.jdkName = Optional.fromNullable(jdkName);
+    arg.jdkName = Optional.ofNullable(jdkName);
     return this;
   }
 
   public ProjectConfigBuilder setJdkType(@Nullable String jdkType) {
-    arg.jdkType = Optional.fromNullable(jdkType);
+    arg.jdkType = Optional.ofNullable(jdkType);
     return this;
   }
 }
